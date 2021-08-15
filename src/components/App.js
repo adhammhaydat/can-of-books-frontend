0d6c2f588441ca6191d4251a44f638b5ac6c1539
adhammhaydat <adhammohidat123@gmail.com>
2021-08-16 00:37:48 +0300

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { withAuth0 } from "@auth0/auth0-react";
+import Login from './Login';
+import MyFavoriteBooks from './BestBooks';
+import Profile from './Profile';
 
 class App extends React.Component {
 
@@ -15,20 +19,27 @@ class App extends React.Component {
     return(
       <>
         <Router>
-          <IsLoadingAndError>
+          
             <Header />
             <Switch>
               <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {/* TODO: if the user is logged in, render the `BestBooks` 
+                component, if they are not, render the `Login` component */}
+                {this.props.auth0.isAuthenticated? <MyFavoriteBooks/>:<Login/>}
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/profile">
+                {this.props.auth0.isAuthenticated &&
+                <Profile />}
+                
+              </Route>
             </Switch>
             <Footer />
-          </IsLoadingAndError>
+          
         </Router>
       </>
     );
   }
 }
 
-export default App;
+export default  withAuth0(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+const dome= 'dev-8365axvp.us.auth0.com'
+const clint="AokBZMisZIJscU161t1mO895UIT9xH2O"
 // TODO: wrap everything in Auth0
 ReactDOM.render(
+  
   <React.StrictMode>
+    <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+
+    clientId={process.env.REACT_APP_CLIENTID}
+    
+    redirectUri={window.location.origin}
+  > 
+  
     <App />
+  </Auth0Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
